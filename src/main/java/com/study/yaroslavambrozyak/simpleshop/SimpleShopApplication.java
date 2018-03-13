a3392dd2a7ed613fa8a66dbc50e95ea8a241af64
yaroslav.ambrozyak <ambrozyakyaroslav@gmail.com>
2018-03-13 18:23:00 +0200

    Initial commit

---
+++ src/main/java/com/study/yaroslavambrozyak/simpleshop/SimpleShopApplication.java
@@ -0,0 +1,12 @@
+package com.study.yaroslavambrozyak.simpleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShopApplication.class, args);
+	}
+}
